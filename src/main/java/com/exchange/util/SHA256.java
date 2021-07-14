877f067ea6cf59c31494e585c43268a05b093e0e
KIMTAEMOK <using1524@naver.com>
2021-07-14 15:31:01 +0900

    SHA256 암호화 util 추가

---
+++ src/main/java/com/exchange/util/SHA256.java
@@ -0,0 +1,25 @@
+package com.exchange.util;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class SHA256 {
+    public static String encryptSHA256(String... values) throws NoSuchAlgorithmException {
+        MessageDigest sha256 = MessageDigest.getInstance("SHA-256");
+        StringBuffer result = new StringBuffer();
+
+        for(String value : values) {
+            result.append(value);
+        }
+
+        byte[] digests = sha256.digest();
+        sha256.update(result.toString().getBytes());
+        result = new StringBuffer();
+
+        for(byte digest : digests) {
+            result.append(Integer.toHexString(digest & 0xFF).toUpperCase());
+        }
+
+        return result.toString();
+    }
+}
